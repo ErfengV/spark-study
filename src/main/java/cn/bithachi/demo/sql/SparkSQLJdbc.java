de5c3e75351e99b839a45f37bd6970846adf3153
bithachi <bitahchi@163.com>
2022-08-24 16:32:36 +0800

    jdbc链接mysql查询数据

---
+++ src/main/java/cn/bithachi/demo/sql/SparkSQLJdbc.java
@@ -0,0 +1,40 @@
+package cn.bithachi.demo.sql;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.Properties;
+
+/**
+ * @Author: BitHachi
+ * @Email: bithachi@163.com
+ * @Date: 2022/8/24
+ * @Description:
+ */
+public class SparkSQLJdbc {
+    public static void main(String[] args) {
+        SparkSession sparkSession = SparkSession.builder()
+                .appName("SparkSQLJdbc")
+                .master("local").getOrCreate();
+
+//        Dataset<Row> load = sparkSession.read().format("jdbc")
+//                .option("url", "jdbc:mysql://localhost:3306/test")
+//                .option("driver", "com.mysql.jdbc.Driver")
+//                .option("query", "select * from user ")
+//                .option("user", "root")
+//                .option("password", "123456")
+//                .load();
+
+        Properties properties = new Properties();
+        properties.setProperty("user","root");
+        properties.setProperty("password","123456");
+        Dataset<Row> load = sparkSession.read().jdbc("jdbc:mysql://localhost:3306/test","user",properties);
+        Dataset<Row> a = load.select("name", "sex");
+        Dataset<Row> b = load.select("name","sex").where("name ='王五'");
+        a.show();
+        b.show();
+
+        load.show();
+    }
+}

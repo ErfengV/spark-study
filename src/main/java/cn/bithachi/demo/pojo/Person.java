1aa638776f7161cf1a9b582e9fcaf51a6fe4fdd4
bithachi <bitahchi@163.com>
2022-08-24 14:03:01 +0800

    Spark SQL的基本使用

---
+++ src/main/java/cn/bithachi/demo/pojo/Person.java
@@ -0,0 +1,55 @@
+package cn.bithachi.demo.pojo;
+
+/**
+ * @Author: BitHachi
+ * @Email: bithachi@163.com
+ * @Date: 2022/8/24
+ * @Description:
+ */
+public class Person {
+    int id;
+    String name;
+    int age;
+
+    public Person() {
+    }
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/cn/bithachi/demo/sql/SparkSQLAgeDesc.java
@@ -0,0 +1,36 @@
+package cn.bithachi.demo.sql;
+
+import cn.bithachi.demo.pojo.Person;
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.sql.*;
+
+/**
+ * @Author: BitHachi
+ * @Email: bithachi@163.com
+ * @Date: 2022/8/24
+ * @Description:
+ */
+public class SparkSQLAgeDesc {
+    public static void main(String[] args) throws AnalysisException {
+        SparkSession sparkSession = SparkSession.builder().appName("SparkSQLAgeDesc").master("local").getOrCreate();
+
+        // 加载数据为DataSet
+        Dataset<String> stringDataset = sparkSession.read().textFile("D:\\code\\IDEA\\SparkStudy\\src\\main\\resources\\other\\person.txt");
+        stringDataset.show();
+
+        // 给Dataset添加元数据信息
+        Dataset<Person> personDataset = stringDataset.map(
+                (MapFunction<String, Person>) line -> {
+                    String[] strings = line.split(",");
+                    return new Person(Integer.valueOf(strings[0]),strings[1],Integer.valueOf(strings[2]));
+                }
+                , Encoders.bean(Person.class));
+        personDataset.show();
+
+        // 创建一个临时视图v_person
+        personDataset.createTempView("v_person");
+        Dataset<Row> dataset = sparkSession.sql("select * from v_person order by age desc");
+        dataset.show();
+
+    }
+}

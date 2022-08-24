83bfa9b70ac9edb62d7c8ef49737684d65cc08f4
bithachi <bitahchi@163.com>
2022-08-24 14:55:03 +0800

    分区自动判断

---
+++ src/main/java/cn/bithachi/demo/sql/PartitionAutoInference.java
@@ -0,0 +1,21 @@
+package cn.bithachi.demo.sql;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+/**
+ * @Author: BitHachi
+ * @Email: bithachi@163.com
+ * @Date: 2022/8/24
+ * @Description: 分区自动判断
+ */
+public class PartitionAutoInference {
+    public static void main(String[] args) {
+        SparkSession sparkSession = SparkSession.builder().appName("SparkSQLAgeDesc").master("local").getOrCreate();
+
+        Dataset<Row> dataset = sparkSession.read().format("json").load("D:\\code\\IDEA\\SparkStudy\\src\\main\\resources\\other\\people");
+        dataset.printSchema();
+        dataset.show();
+    }
+}
